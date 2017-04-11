76fd26b55076ecbee8280a8c06fd1ffdb8291e8a
Terence Tham <terence@epiphanyedu.com>
2017-04-10 17:10:21 -0700

    Add Twilio calling from GET request to server

--- server/calling/callingHandler.js
+++ server/calling/callingHandler.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const Call = require('./config.js');
+const xml = require('xml');
 
-router.get('/call', (req, res) => {
+router.post('/call', (req, res) => {
+	// Call.call();
 	res.status(201).send();
 });
+
+router.post('/called', (req, res) => {
+	res.status(200).send(xml({a: 'ok'}));
+})
 
 module.exports = router;
