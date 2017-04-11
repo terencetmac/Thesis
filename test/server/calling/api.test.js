772fdb3ea0b72091847dc72d257edb204c68febb
Terence Tham <terence@epiphanyedu.com>
2017-04-10 17:23:45 -0700

    Try circleci fix

--- test/server/calling/api.test.js
+++ test/server/calling/api.test.js
@@ -1,5 +1,5 @@
-jest.disableAutomock();
-jest.unmock('supertest').unmock('../../../server/server.js');
+// jest.disableAutomock();
+// jest.unmock('supertest');
 
 // import request from 'supertest-as-promised';
 // import express from 'express';
@@ -7,13 +7,13 @@ let app;
 
 describe('Calling API tests', () => {
 	beforeEach(() => {
- 		process.env.NODE_ENV = 'test';
- 	//	app = require('../../../server/server.js');
+ 		// process.env.NODE_ENV = 'test';
+ 		// app = require('../../../server/server.js');
 	});
 
 	afterEach(() => {
 		//app.close(done);
-	//	delete process.env.NODE_ENV;
+		// delete process.env.NODE_ENV;
 	});
 
 	it('should run a test', (done) => {
