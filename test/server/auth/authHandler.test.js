6802f0c70acf3ac518a7fcabc35b53bf34a1a238
Terence Tham <terence@epiphanyedu.com>
2017-04-13 17:19:07 -0700

    Last

--- test/server/auth/authHandler.test.js
+++ test/server/auth/authHandler.test.js
@@ -1,4 +1,4 @@
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
 require('dotenv').config();
 import request from 'supertest';
 import { app } from '../../../server/server.js';
@@ -18,7 +18,7 @@ describe('authHandler tests', () => {
 		expect(1).toBe(1);
 	})	
 
-	test('should handle POST /signup route', () => {
+	test('should handle POST /signup route', (done) => {
 		return resetDb().then(() => {
 			return request(app).post('/api/auth/signup')
 				.send({
@@ -33,75 +33,76 @@ describe('authHandler tests', () => {
 			.then((res) => {
 				expect(res.body.user).toBeDefined();
 				expect(res.body.token).toBeDefined();
+				done();
 			});
 	});
 
-	// test('should send an error message if email exists in the DB', (done) => {
-	// 	return resetDb().then(() => {
-	// 		return request(app).post('/api/auth/signup')
-	// 			.send({
-	// 				email: 'newUser@mail.com',
-	// 				firstName: 'New user',
-	// 				lastName: 'To be deleted',
-	// 				password: 'password',
-	// 				phone: '7582931276'
-	// 			})
-	// 	})
-	// 		.then(() => {
-	// 			return request(app).post('/api/auth/signup')
-	// 				.send({
-	// 					email: 'newUser@mail.com',
-	// 					firstName: 'New user',
-	// 					lastName: 'To be deleted',
-	// 					password: 'password',
-	// 					phone: '7582931276'
-	// 				})
-	// 				.expect(400)
-	// 		})
-	// 		.then(res => {
-	// 			expect(res.error.text).toBeDefined();
-	// 			done();
-	// 		});
-	// });
+	test('should send an error message if email exists in the DB', (done) => {
+		return resetDb().then(() => {
+			return request(app).post('/api/auth/signup')
+				.send({
+					email: 'newUser@mail.com',
+					firstName: 'New user',
+					lastName: 'To be deleted',
+					password: 'password',
+					phone: '7582931276'
+				})
+		})
+			.then(() => {
+				return request(app).post('/api/auth/signup')
+					.send({
+						email: 'newUser@mail.com',
+						firstName: 'New user',
+						lastName: 'To be deleted',
+						password: 'password',
+						phone: '7582931276'
+					})
+					.expect(400)
+			})
+			.then(res => {
+				expect(res.error.text).toBeDefined();
+				done();
+			});
+	});
 
-	// test('should handle POST /login route', (done) => {
-	// 	return resetDb().then(() => {
-	// 		return request(app).post('/api/auth/signup')
-	// 			.send({
-	// 				email: 'newUser@mail.com',
-	// 				firstName: 'New user',
-	// 				lastName: 'To be deleted',
-	// 				password: 'password',
-	// 				phone: '7582931276'
-	// 			});
-	// 	})
-	// 	.then(() => {
-	// 		return request(app).post('/api/auth/login')
-	// 		.send({
-	// 			email: 'newUser@mail.com',
-	// 			password: 'password'
-	// 		})
-	// 		.expect(200)
-	// 	})
-	// 	.then(res => {
-	// 		expect(res.body.user).toBeDefined();
-	// 		expect(res.body.token).toBeDefined();
-	// 		done();
-	// 	});
-	// });
+	test('should handle POST /login route', (done) => {
+		return resetDb().then(() => {
+			return request(app).post('/api/auth/signup')
+				.send({
+					email: 'newUser@mail.com',
+					firstName: 'New user',
+					lastName: 'To be deleted',
+					password: 'password',
+					phone: '7582931276'
+				});
+		})
+		.then(() => {
+			return request(app).post('/api/auth/login')
+			.send({
+				email: 'newUser@mail.com',
+				password: 'password'
+			})
+			.expect(200)
+		})
+		.then(res => {
+			expect(res.body.user).toBeDefined();
+			expect(res.body.token).toBeDefined();
+			done();
+		});
+	});
 
-	// test('should send an error message if login fails.', (done) => {
-	// 	return request(app).post('/api/auth/login')
-	// 		.send({
-	// 			email: 'newUser@mail.com',
-	// 			password: 'wrongpassword'
-	// 		})
-	// 		.expect(401)
-	// 		.then(res => {
-	// 			expect(res.error.text).toBeDefined();
-	// 			done();
-	// 		});
-	// });
+	test('should send an error message if login fails.', (done) => {
+		return request(app).post('/api/auth/login')
+			.send({
+				email: 'newUser@mail.com',
+				password: 'wrongpassword'
+			})
+			.expect(401)
+			.then(res => {
+				expect(res.error.text).toBeDefined();
+				done();
+			});
+	});
 
 	/**
 	 * 3rd party API call route, not testing for now
